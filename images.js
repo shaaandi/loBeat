704ecbc48590aeb569c7da962c1aa62b2116f3a7
Shaaandi <deviloversaad@gmail.com>
2019-06-21 01:53:56 +0500

    After all ,

---
+++ images.js
@@ -0,0 +1,22 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("images");
+  var captions = document.getElementsByClassName('captions')
+  for (i = 0; i < slides.length; i++) {
+    // slides[i].style.width = "0";  
+    slides[i].style.height = "0"; 
+    captions[i].style.fontSize = '0';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+//   slides[slideIndex-1].style.width = "100%"; 
+  slides[slideIndex-1].style.height = "500px";  
+  captions[slideIndex-1].style.fontSize = "30"
+  setTimeout(showSlides, 3500); 
+}
+
+
+
